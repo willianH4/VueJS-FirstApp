cae577662c26b9ba8d08e466243b194ea3c8adca
Willian <williamhernandez450@gmail.com>
2023-09-05 07:10:30 -0600

    Hello World

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  // logic or components
+</script>
+
+<template>
+  <!-- html -->
+  <h1>Hola Mundo</h1>
+</template>
+
+<style scoped>
+  /* styles for component */
+  h1 {
+    color: green;
+  }
+
+</style>

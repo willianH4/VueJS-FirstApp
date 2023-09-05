de213806736e136a85adaf8ffc3f912b23706290
Willian <williamhernandez450@gmail.com>
2023-09-05 07:39:24 -0600

    Create firsts components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
   // logic or components
+  import PostContainer from './components/post/PostContainer.vue';
 </script>
 
 <template>
   <!-- html -->
-  <h1>Hola Mundo</h1>
+  <h1>Listado de posts:</h1>
+  <PostContainer />
 </template>
 
 <style scoped>

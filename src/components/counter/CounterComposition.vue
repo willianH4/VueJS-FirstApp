bb19eff5331f91bda39bbe50d1a5a2dd52588065
Willian <williamhernandez450@gmail.com>
2023-09-06 05:17:00 -0600

    Declare type of data

--- src/components/counter/CounterComposition.vue
+++ src/components/counter/CounterComposition.vue
@@ -4,13 +4,13 @@
 </template>
 
 <script lang="ts">
-    import { defineComponent, ref } from 'vue';
+    import { defineComponent, ref, type Ref } from 'vue';
 
     export default defineComponent({
         name: 'CounterComposition',
         setup() {
-            let counter = ref(0)
-            const addCounter = () => {
+            let counter: Ref = ref(0)
+            const addCounter = ():void => {
                 counter.value++
             }
             return { counter, addCounter }

b2d3511b29d3eeb4dd5d8a40b95ae36d7290eae7
Willian <williamhernandez450@gmail.com>
2023-09-06 05:49:27 -0600

    Using directive v-show

--- src/components/directives/ExampleSelective.vue
+++ src/components/directives/ExampleSelective.vue
@@ -1,6 +1,6 @@
 <template>
-    <h1 v-if="age < 18">Eres menor de edad</h1>
-    <h1 v-else>Eres mayor de edad</h1>
+    <h1 v-show="age < 18">Eres menor de edad</h1>
+    <h1 v-show="age > 18">Eres mayor de edad</h1>
 </template>
 
 <script lang="ts" setup>

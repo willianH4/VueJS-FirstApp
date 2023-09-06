8f7af3be3f1b1ba948760c860924d419d4695bbf
Willian <williamhernandez450@gmail.com>
2023-09-06 05:21:30 -0600

    Implement setup in component CounterComposition

--- src/components/counter/CounterComposition.vue
+++ src/components/counter/CounterComposition.vue
@@ -3,17 +3,11 @@
     <button @click="addCounter">Incrementar</button>
 </template>
 
-<script lang="ts">
-    import { defineComponent, ref, type Ref } from 'vue';
-
-    export default defineComponent({
-        name: 'CounterComposition',
-        setup() {
-            let counter: Ref = ref(0)
-            const addCounter = ():void => {
-                counter.value++
-            }
-            return { counter, addCounter }
-        }
-    })
+<script lang="ts" setup>
+    import { ref, type Ref } from 'vue';
+    
+    let counter: Ref = ref(0)
+    const addCounter = ():void => {
+        counter.value++
+    }
 </script>

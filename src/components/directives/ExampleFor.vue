320809c06f06da88ac592ccf392d3179da47404c
Willian <williamhernandez450@gmail.com>
2023-09-06 06:15:28 -0600

    Using directive v-for iterate structure array of object

--- src/components/directives/ExampleFor.vue
+++ src/components/directives/ExampleFor.vue
@@ -1,14 +1,26 @@
 <template>
     <ul>
-        <li v-for="(country, index) in countries" :key="index">{{ country }}</li>
+        <li v-for="(country, index) in countries" :key="index">{{ country.name }}</li>
     </ul>
 </template>
 
 <script lang="ts" setup>
+    import type { ICountry } from '@/data/interfaces/Country';
     import { ref, type Ref } from 'vue';
     
-    let countries:Ref<Array<string>> = ref([
-        'Spain', 'France', 'UK', 'Italy', 'Portugal'
+    let countries:Ref<Array<ICountry>> = ref([
+        {
+            code: 'es',
+            name: 'Spain'
+        },
+        {
+            code: 'en',
+            name: 'England'
+        },
+        {
+            code: 'sv',
+            name: 'El Salvador'
+        }
     ])
 </script>
 

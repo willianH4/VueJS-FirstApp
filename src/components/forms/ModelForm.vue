719299e2e2e5a6362a3f33686d788b6b7e5a45fd
Willian <williamhernandez450@gmail.com>
2023-09-07 04:59:09 -0600

    Using v-model with selects and composition API + setup

--- src/components/forms/ModelForm.vue
+++ src/components/forms/ModelForm.vue
@@ -1,18 +1,22 @@
 <template>
     <form>
         <input type="text" v-model="inputText">
+        <input type="checkbox" name="check" v-model="agree"> Acepto terminos y condiciones
+        <select v-model="country">
+            <option value="Spain">Spain</option>
+            <option value="Mexico">Mexico</option>
+            <option value="El Salvador">El Salvador</option>
+        </select>
     </form>
     <h3>{{ inputText }}</h3>
 </template>
 
-<script lang="ts">
-    import { defineComponent, ref } from 'vue';
+<script lang="ts" setup>
+    import { ref } from 'vue';
 
-    export default defineComponent({
-        data() {
-            return { inputText: '' }
-        }
-    })
+    let inputText = ref('')
+    let agree = ref(true)
+    let country = ref('El Salvador')
 </script>
 
 <style scoped>
